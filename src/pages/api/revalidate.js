2816aa9f67d751afbf3d0cefce5872e15538576c
trung1051996 <trung.nguyen@snaptec.co>
2023-05-18 11:17:03 +0700

    NextJS 02-10: ISR

---
+++ src/pages/api/revalidate.js
@@ -0,0 +1,18 @@
+export default async function handler(req, res) {
+    // Check for secret to confirm this is a valid request
+    if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+      return res.status(401).json({ message: 'Invalid token' });
+    }
+   
+    try {
+      // this should be the actual path not a rewritten path
+      // e.g. for "/blog/[slug]" this should be "/blog/post-1"
+      await res.revalidate('/posts/lea11ziflg8xoixq');
+      return res.json({ revalidated: true, postId: req.query.postId });
+    } catch (err) {
+        // If there was an error, Next.js will continue
+        // to show the last successfully generated page
+        console.log("🚀 ~ file: revalidate.js:13 ~ handler ~ err:", err)
+      return res.status(500).send('Error revalidating');
+    }
+  }
--- src/pages/posts/[postId].js
+++ src/pages/posts/[postId].js
@@ -4,6 +4,10 @@ import React from 'react';
 const PostDetailPage = ({ post }) => {
     const router = useRouter()
 
+    if (router.isFallback) {
+
+    }
+    
     if (!post) return null;
 
     return (
@@ -16,31 +20,34 @@ const PostDetailPage = ({ post }) => {
 
 export default PostDetailPage;
 
-
-export const getStaticProps = async (context) => {
-    if(!context.params?.postId) return {
-        notFound: true
-    }
-
-    const response = await fetch(`https://6295e38d75c34f1f3b23e68f.mockapi.io/api/v1/blogposts/${context.params?.postId}`)
-    const data = await response.json();
-    
-    return {
-        props: {
-            post: data
-        }
-    }
-}
-
 export const getStaticPaths = async () => {
 
-    const response = await fetch("https://6295e38d75c34f1f3b23e68f.mockapi.io/api/v1/blogposts")
+    // const response = await fetch("https://6295e38d75c34f1f3b23e68f.mockapi.io/api/v1/blogposts")
+    const response = await fetch("https://js-post-api.herokuapp.com/api/posts?_page=1")
     const data = await response.json();
     console.log("\ngetStaticPaths", data);
 
     
     return {
-        paths: data.map(post => ({ params: { postId: post.id }})),
-        fallback: false,
+        paths: data.data.map(post => ({ params: { postId: post.id }})),
+        fallback: 'blocking',
+    }
+}
+
+export const getStaticProps = async (context) => {
+    console.log("🚀 getStaticProps ~ context.params?.postId:", context.params?.postId)
+    if(!context.params?.postId) return {
+        notFound: true
+    }
+
+    // const response = await fetch(`https://6295e38d75c34f1f3b23e68f.mockapi.io/api/v1/blogposts/${context.params?.postId}`)
+    const response = await fetch(`https://js-post-api.herokuapp.com/api/posts/${context.params?.postId}`)
+    const data = await response.json();
+    
+    return {
+        props: {
+            post: data
+        },
+        revalidate: 15
     }
 }
